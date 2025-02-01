80361db1a0c356602419022cb6e921dea17f9e0a
luffexa1 <luffexa1@gmail.com>
2025-02-01 21:02:15 +0400

    commit

---
+++ day00/homework/lesson0.py
@@ -0,0 +1,52 @@
+from turtle import *
+
+#we want to paint a house
+
+#step 1:  draw a square
+
+speed(30)
+width(7)
+color("green")
+begin_fill()
+forward(200)
+left(90)
+
+forward(200)
+left(90)
+
+forward(200)
+left(90)
+
+forward(200)
+left(90)
+end_fill()
+#end of square
+
+#drawing a door
+
+forward(70)
+begin_fill()
+color("blue")
+left(90)
+forward(120)     #height of the door
+right(90)
+forward(60)
+right(90)
+forward(120)
+end_fill()
+
+penup()
+goto(200, 200)
+pendown()
+
+color("red")
+begin_fill()
+right(150)
+forward(200)
+left(120)
+forward(200)
+end_fill()
+
+
+
+exitonclick()
---
+++ day01/homework/hw.py
@@ -0,0 +1,6 @@
+name = "luka"
+surname = "beridze"
+age = "18"
+height = "1.85"
+weight = "66"
+print("chemi sakhelia:", name, surname, "chemi asakia:", age, "chemi simaglea:", height, "chemi wona aris:", weight,)
